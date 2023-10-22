3d0453fafee86898a9b3c91c179c797bfe546045
PayRequest <abuse@hostingwalk.com>
2023-10-22 08:16:33 -0500

    pages

--- docs/api/payrequest-api.md
+++ docs/api/payrequest-api.md
@@ -2,7 +2,7 @@
 sidebar_position: 1
 ---
 
-# Create a Page
+# PayRequest API
 
 Add **Markdown or React** files to `src/pages` to create a **standalone page**:
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/api">
-            Discover the PayRequest API
+            Go to our Documentation
           </Link>
         </div>
       </div>
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <Layout
       title={` ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="PayRequest Developer and API">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
